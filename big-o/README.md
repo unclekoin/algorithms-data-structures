0c18b2d978a30fcf31dedcf583918091fa9d6430
Pavel Koryakin <unclekoin@gmail.com>
2021-12-09 13:35:16 +0300

    feat: add big O terms

---
+++ big-o/README.md
@@ -0,0 +1,14 @@
+# Big O
+[Big-O Cheat Sheet](https://www.bigocheatsheet.com/)
+### Terms
+* Time Complexity
+* Space Complexity
+* Worst Case
+* Drop Constants
+* Drop Non-Dominants
+
+---
+* O(n^2) - loop within a loop
+* O(n) - proportional
+* O(log n) - divide and conquer
+* O(1) - constant
--- data-strucrures/linked-lists/linkedlist-class.js
+++ data-strucrures/linked-lists/linkedlist-class.js
@@ -96,21 +96,63 @@ class LinkedList {
     return false;
   }
 
-  insert(index, value) {}
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
 
-  remove() {}
+    const node = new Node(value);
+    const temp = this.get(index - 1);
 
-  reverse() {}
+    node.next = temp.next;
+    temp.next = node;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const before = this.get(index - 1);
+    const temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(5);
 
-// myLinkedList.push(23);
-// myLinkedList.push(2);
-// myLinkedList.push(10);
+myLinkedList.push(23);
+myLinkedList.push(2);
+myLinkedList.push(10);
 // console.log(myLinkedList.pop());
 // myLinkedList.unshift(7);
 // console.log(myLinkedList.shift());
 // console.log(myLinkedList.get(3));
 // console.log(myLinkedList.set(1, 55));
-console.log(myLinkedList);
+// console.log(myLinkedList.insert(2, 77));
+// console.log(myLinkedList.remove(2));
+// console.log(JSON.stringify(myLinkedList.head, null, ' '));
+// console.log(myLinkedList.length)
+console.log(myLinkedList.reverse())
