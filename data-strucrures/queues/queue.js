b4602018c01651d7b9b816acdc660b7da6de5768
Pavel Koryakin <unclekoin@gmail.com>
2021-12-17 15:44:09 +0300

    feat: add queue

--- data-strucrures/queues/queue.js
+++ data-strucrures/queues/queue.js
@@ -12,4 +12,46 @@ class Queue {
     this.last = node;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const node = new Node(value);
+    if (!this.length) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.length) return undefined;
+
+    let temp = this.first;
+
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
+
+const queue = new Queue(1);
+
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+
+console.log(queue);
